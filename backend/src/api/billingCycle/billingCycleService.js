ee04f1c9bbd0b2a4d9a9cd84a659db92e7023927
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-09-02 08:52:48 -0300

    fix: get route was not working

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -2,5 +2,15 @@ const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
+BillingCycle.route('get', (_req, res, _next) => {
+  BillingCycle.find({}, (err, docs) => {
+    if (!err) {
+      res.json(docs)
+    } else {
+      res.status(500).json({ errors: [err] })
+    }
+  })
+})
+
 
 module.exports = BillingCycle
