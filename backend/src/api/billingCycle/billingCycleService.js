e711b612dd40bbed6a22f5e18b8b260217f5b9bc
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-09-03 09:02:55 -0300

    feat: add count end point

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -12,5 +12,15 @@ BillingCycle.route('get', (_req, res, _next) => {
   })
 })
 
+BillingCycle.route('count', (req, res, next) => {
+  BillingCycle.count((error, value) => {
+    if (error) {
+      res.status(500).json({ errors: [error] })
+    } else {
+      res.json({ value })
+    }
+  })
+})
+
 
 module.exports = BillingCycle
