40608012010f2385ec8e435aaa43f0792e32293e
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-09-08 09:54:28 -0300

    add error handler and disable cors(with problem)

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,6 @@
-module.exports = (req, res, next) => {
+module.exports = (_req, res, next) => {
   res.render('Access-Control-Allow-Origin', '*')
-  res.render('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATH')
-  res.render('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type Accept')
+  res.render('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.render('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept')
   next()
 }
