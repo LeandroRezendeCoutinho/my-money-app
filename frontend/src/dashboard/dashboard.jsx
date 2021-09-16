4a02e401542f5b75c0db7384f60fd9f8df8d2322
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-09-16 09:18:42 -0300

    feat: add row componet
    row is optional to organize layout

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import ContenHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
 import ValueBox from "../common/widget/valueBox"
+import Row from "../common/layout/row"
 
 class Dashboard extends Component {
   render() {
@@ -10,12 +11,14 @@ class Dashboard extends Component {
       <div>
         <ContenHeader title="Dashboard" small='Versão 1.0' />
         <Content>
-          <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10'
-            text='Total credits' />
-          <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10'
-            text='Total debits' />
-          <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0'
-            text='Consolidated total ' />
+          <Row>
+            <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10'
+              text='Total credits' />
+            <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10'
+              text='Total debits' />
+            <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0'
+              text='Consolidated total' />
+          </Row>
         </Content>
       </div>
     )
