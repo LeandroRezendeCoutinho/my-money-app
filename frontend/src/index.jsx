c4ed16695a7e6162936d1b6f099c451721f1f179
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-09-20 09:15:48 -0300

    feat: add redux action do dashboad

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
+import promise from "redux-promise";
 
 import App from "./main/app.jsx";
 import reducers from "./main/reducers";
 
-const store = createStore(reducers)
+// applyMiddleware retorna uma função que recebe createStore como parametro
+// que retorna outra função que recebe reducers como parametro
+const store = applyMiddleware(promise)(createStore)(reducers)
 ReactDOM.render(
   <Provider store={store}>
     <App />
