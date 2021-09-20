df32d79e8ea4d5acd1706ec8550556b1af6f5bcc
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-09-20 07:52:46 -0300

    feat: create dashboard reducer

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import ContenHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
@@ -7,16 +8,17 @@ import Row from "../common/layout/row"
 
 class Dashboard extends Component {
   render() {
+    const { credit, debt } = this.props.summary
     return (
       <div>
         <ContenHeader title="Dashboard" small='Versão 1.0' />
         <Content>
           <Row>
-            <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10'
+            <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`}
               text='Total credits' />
-            <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10'
+            <ValueBox cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`}
               text='Total debits' />
-            <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0'
+            <ValueBox cols='12 4' color='blue' icon='money' value={`R$ ${credit - debt}`}
               text='Consolidated total' />
           </Row>
         </Content>
@@ -25,4 +27,5 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard
+const mapStateToProps = (state) => ({ summary: state.dashboard.summary })
+export default connect(mapStateToProps)(Dashboard)
